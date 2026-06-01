acb684931ad3c068f4554335fed95ba8bb4fe036
Eman Sajjad <akzachoudhry1123@gmail.com>
2026-06-01 15:02:38 +0500

    nre data pushed

--- index.js
+++ index.js
@@ -3,6 +3,41 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const githubData = {
+  "login": "octocat",
+  "id": 583231,
+  "node_id": "MDQ6VXNlcjU4MzIzMQ==",
+  "avatar_url": "https://avatars.githubusercontent.com/u/583231?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/octocat",
+  "html_url": "https://github.com/octocat",
+  "followers_url": "https://api.github.com/users/octocat/followers",
+  "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+  "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+  "organizations_url": "https://api.github.com/users/octocat/orgs",
+  "repos_url": "https://api.github.com/users/octocat/repos",
+  "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/octocat/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "The Octocat",
+  "company": "@github",
+  "blog": "https://github.blog",
+  "location": "San Francisco",
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 8,
+  "public_gists": 8,
+  "followers": 22815,
+  "following": 9,
+  "created_at": "2011-01-25T18:44:36Z",
+  "updated_at": "2026-05-22T11:38:32Z"
+}
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
@@ -10,6 +45,10 @@ app.get('/', (req, res) => {
 app.get('/twitter' , (req, res) => {
     res.send('imandotcom')
 } )
+
+app.get('/github' , (req, res) => {
+  res.json(githubData)
+})
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
 });
